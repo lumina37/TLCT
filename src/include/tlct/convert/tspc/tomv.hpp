60e305423cd713ea087d16d713b02b7f4c7fe5ba
SIGS-TZ <tz23@mails.tsinghua.edu.cn>
2024-05-11 09:06:28 +0800

    refactor: strip some stale vars

--- src/include/tlct/convert/tspc/tomv.hpp
+++ src/include/tlct/convert/tspc/tomv.hpp
@@ -24,11 +24,9 @@ TLCT_API inline void _Lenslet_Rendering_zoom(const cv::Mat& src, const cfg::tspc
 
     constexpr int zoom = 3;
     constexpr int zoomto_height = 20 * zoom; // the extracted patch will be zoomed to this height
-    constexpr int zoomto_width = (int)((double)zoomto_height * std::numbers::sqrt3 / 2.0 + 0.5);
+    constexpr int zoomto_width = iround((double)zoomto_height * std::numbers::sqrt3 / 2.0);
     constexpr int bound = zoom;
     constexpr int zoomto_withbound = zoomto_height + 2 * bound;
-    constexpr int q = zoomto_withbound / 2;
-    constexpr int q1 = zoomto_height / 2;
 
     cv::Mat d_src; // convert src from 8UCn to 64FCn
     src.convertTo(d_src, CV_64FC3);
@@ -39,8 +37,6 @@ TLCT_API inline void _Lenslet_Rendering_zoom(const cv::Mat& src, const cfg::tspc
         rgs::views::iota(-views / 2, views / 2 + 1) | rgs::views::transform([interval](int x) { return x * interval; });
     auto rowviews = colviews;
 
-    const cv::Size mi_nums = {layout.getMICols(), layout.getMIRows()};
-
     const cv::Point center_0_0 = layout.getMICenter(0, 0);
     const cv::Point center_0_1 = layout.getMICenter(0, 1);
     const bool is_out_shift = center_0_1.y < center_0_0.y;
@@ -48,14 +44,14 @@ TLCT_API inline void _Lenslet_Rendering_zoom(const cv::Mat& src, const cfg::tspc
     int img_cnt = 0;
     for (const int colview : colviews) {
         for (const int rowview : rowviews) {
-            const int canvas_height = (mi_nums.height - 1) * zoomto_height + 2 * bound + q1;
-            const int canvas_width = mi_nums.width * zoomto_width + zoomto_withbound - zoomto_width;
+            const int canvas_height = (layout.getMIRows() - 1) * zoomto_height + 2 * bound + zoomto_height / 2;
+            const int canvas_width = layout.getMICols() * zoomto_width + zoomto_withbound - zoomto_width;
             cv::Mat render_canvas = cv::Mat::zeros(canvas_height, canvas_width, CV_64FC3);
             cv::Mat weight_canvas = cv::Mat::zeros(canvas_height, canvas_width, CV_64FC1);
             const cv::Mat weight_template = cv::Mat::ones({zoomto_withbound, zoomto_withbound}, CV_64FC1);
 
-            for (const int i : rgs::views::iota(0, mi_nums.width - 1)) {
-                for (const int j : rgs::views::iota(0, mi_nums.height - 1)) {
+            for (const int i : rgs::views::iota(0, layout.getMICols() - 1)) {
+                for (const int j : rgs::views::iota(0, layout.getMIRows() - 1)) {
                     const cv::Point center = layout.getMICenter(j, i);
                     if (center.x == 0 or center.y == 0)
                         continue;
@@ -65,7 +61,7 @@ TLCT_API inline void _Lenslet_Rendering_zoom(const cv::Mat& src, const cfg::tspc
                     const int psize = ori_psize * zoom;
                     const int psize_with_bound = psize + 2 * bound;
                     // you would need some residual for a bicubic zoom
-                    const int zoom_residual = (ori_psize + 1) / 2 + 3;
+                    const int zoom_residual = (int)std::ceil((double)ori_psize / 2.0) + 3;
 
                     const cv::Range patch_with_resi_row_range{center.y - zoom_residual + colview - 1,
                                                               center.y + zoom_residual + colview - 1};
@@ -90,14 +86,15 @@ TLCT_API inline void _Lenslet_Rendering_zoom(const cv::Mat& src, const cfg::tspc
                     // if the second bar is not out shift, then we need to shift the 1 col
                     // else if the second bar is out shift, then we need to shift the 0 col
                     const int down_shift = (i % 2) ^ (int)is_out_shift;
-                    cv::Rect roi{i * zoomto_width, j * zoomto_height + q1 * down_shift, zoomto_withbound,
+                    cv::Rect roi{i * zoomto_width, j * zoomto_height + zoomto_height / 2 * down_shift, zoomto_withbound,
                                  zoomto_withbound};
                     render_canvas(roi) += rotated_patch;
                     weight_canvas(roi) += weight_template;
                 }
             }
 
-            const cv::Range crop_roi[]{cv::Range{q, render_canvas.rows - q}, cv::Range::all()};
+            const cv::Range crop_roi[]{cv::Range{zoomto_withbound / 2, render_canvas.rows - zoomto_withbound / 2},
+                                       cv::Range::all()};
             cv::Mat cropped_new_image = render_canvas(crop_roi);
             cv::Mat cropped_weight_matrix = weight_canvas(crop_roi);
             cropped_weight_matrix.setTo(1.0, cropped_weight_matrix == 0.0);
