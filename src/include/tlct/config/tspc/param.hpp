5827c15f4e8356bf1bf9e8872334ccf50c2eb18d
SIGS-TZ <tz23@mails.tsinghua.edu.cn>
2024-05-14 19:52:12 +0800

    feat: add property `imgsize` for `::tspc::ParamConfig`

--- src/include/tlct/config/tspc/param.hpp
+++ src/include/tlct/config/tspc/param.hpp
@@ -17,18 +17,19 @@ namespace fs = std::filesystem;
 class ParamConfig
 {
 public:
-    TLCT_API ParamConfig() : calib_cfg_(), views_(0), range_(), src_pattern_(), dst_pattern_() {};
-    TLCT_API ParamConfig(const ParamConfig& cfg) = default;
-    TLCT_API ParamConfig(ParamConfig&& cfg) = default;
-    TLCT_API ParamConfig(CalibConfig&& calib_cfg, int views, cv::Range range, std::string src_pattern,
+    TLCT_API ParamConfig() : calib_cfg_(), views_(0), imgsize_(), range_(), src_pattern_(), dst_pattern_() {};
+    TLCT_API ParamConfig(const ParamConfig& config) = default;
+    TLCT_API ParamConfig(ParamConfig&& config) = default;
+    TLCT_API ParamConfig(CalibConfig&& calib_cfg, int views, cv::Size imgsize, cv::Range range, std::string src_pattern,
                          std::string dst_pattern)
-        : calib_cfg_(calib_cfg), views_(views), range_(range), src_pattern_(std::move(src_pattern)),
+        : calib_cfg_(calib_cfg), views_(views), imgsize_(imgsize), range_(range), src_pattern_(std::move(src_pattern)),
           dst_pattern_(std::move(dst_pattern)) {};
 
-    [[nodiscard]] TLCT_API static ParamConfig fromCommonCfg(const CommonParamConfig& common_cfg);
+    [[nodiscard]] TLCT_API static ParamConfig fromCommonCfg(const CommonParamConfig& config);
 
     [[nodiscard]] TLCT_API const CalibConfig& getCalibCfg() const noexcept;
     [[nodiscard]] TLCT_API int getViews() const noexcept;
+    [[nodiscard]] TLCT_API cv::Size getImgSize() const noexcept;
     [[nodiscard]] TLCT_API cv::Range getRange() const noexcept;
     [[nodiscard]] TLCT_API const std::string& getSrcPattern() const noexcept;
     [[nodiscard]] TLCT_API fs::path getSrcPath(int i) const noexcept;
@@ -38,26 +39,31 @@ public:
 private:
     CalibConfig calib_cfg_;
     int views_;
+    cv::Size imgsize_;
     cv::Range range_;
     std::string src_pattern_;
     std::string dst_pattern_;
 };
 
-inline ParamConfig ParamConfig::fromCommonCfg(const CommonParamConfig& common_cfg)
+inline ParamConfig ParamConfig::fromCommonCfg(const CommonParamConfig& config)
 {
-    const auto& cfg_map = common_cfg.getConfigMap();
+    const auto& cfg_map = config.getConfigMap();
     auto calib_cfg = CalibConfig::fromXMLPath(cfg_map.at("Calibration_xml").c_str());
     const int views = std::stoi(cfg_map.at("viewNum"));
+    const int width = std::stoi(cfg_map.at("width"));
+    const int height = std::stoi(cfg_map.at("height"));
     const int start = std::stoi(cfg_map.at("start_frame"));
     const int end = std::stoi(cfg_map.at("end_frame"));
     const std::string& src_pattern = cfg_map.at("RawImage_Path");
     const std::string& dst_pattern = cfg_map.at("Output_Path");
-    return {std::move(calib_cfg), views, {start, end}, src_pattern, dst_pattern};
+    return {std::move(calib_cfg), views, {width, height}, {start, end}, src_pattern, dst_pattern};
 }
 
 inline const CalibConfig& ParamConfig::getCalibCfg() const noexcept { return calib_cfg_; }
 
 inline int ParamConfig::getViews() const noexcept { return views_; }
+
+inline cv::Size ParamConfig::getImgSize() const noexcept { return imgsize_; }
 
 inline cv::Range ParamConfig::getRange() const noexcept { return range_; }
 
