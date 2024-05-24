5c161f944b5c6374ddb09f51ebaeca66f5e0e826
SIGS-TZ <tz23@mails.tsinghua.edu.cn>
2024-05-24 21:43:17 +0800

    feat: `TLCT_HEADER_ONLY` prior than any other integrate mode

--- src/include/tlct/common/defines.h
+++ src/include/tlct/common/defines.h
@@ -2,24 +2,22 @@
 
 #include "cmake.h"
 
-#ifdef TLCT_BUILD_SHARED_LIBS
-#    ifdef _MSC_VER
-#        ifdef _TLCT_BUILD_LIBS
-#            define TLCT_API __declspec(dllexport)
-#        else
-#            define TLCT_API __declspec(dllimport)
-#        endif
-#    else
-#        ifdef _TLCT_BUILD_LIBS
-#            define TLCT_API __attribute__((visibility("default")))
-#        else
-#            define TLCT_API
-#        endif
-#    endif
+#ifdef TLCT_HEADER_ONLY
+#    define TLCT_API static
 #else
-#    ifdef TLCT_HEADER_ONLY
-#        define TLCT_API static
-#    else
-#        define TLCT_API
+#    ifdef TLCT_BUILD_SHARED_LIBS
+#        ifdef _MSC_VER
+#            ifdef _TLCT_BUILD_LIBS
+#                define TLCT_API __declspec(dllexport)
+#            else
+#                define TLCT_API __declspec(dllimport)
+#            endif
+#        else
+#            ifdef _TLCT_BUILD_LIBS
+#                define TLCT_API __attribute__((visibility("default")))
+#            else
+#                define TLCT_API
+#            endif
+#        endif
 #    endif
 #endif
