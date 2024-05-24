e5bc1d869fc5012f55cda46b392e70f8f5b5135a
SIGS-TZ <tz23@mails.tsinghua.edu.cn>
2024-05-24 23:33:21 +0800

    fix: header-only mode

--- src/include/tlct/common/defines.h
+++ src/include/tlct/common/defines.h
@@ -2,22 +2,20 @@
 
 #include "cmake.h"
 
-#ifdef TLCT_HEADER_ONLY
-#    define TLCT_API static
-#else
-#    ifdef TLCT_BUILD_SHARED_LIBS
-#        ifdef _MSC_VER
-#            ifdef _TLCT_BUILD_LIBS
-#                define TLCT_API __declspec(dllexport)
-#            else
-#                define TLCT_API __declspec(dllimport)
-#            endif
+#define TLCT_API
+
+#if !defined(TLCT_HEADER_ONLY) && defined(TLCT_BUILD_SHARED_LIBS)
+#    ifdef _MSC_VER
+#        ifdef _TLCT_BUILD_LIBS
+#            define TLCT_API __declspec(dllexport)
 #        else
-#            ifdef _TLCT_BUILD_LIBS
-#                define TLCT_API __attribute__((visibility("default")))
-#            else
-#                define TLCT_API
-#            endif
+#            define TLCT_API __declspec(dllimport)
+#        endif
+#    else
+#        ifdef _TLCT_BUILD_LIBS
+#            define TLCT_API __attribute__((visibility("default")))
+#        else
+#            define TLCT_API
 #        endif
 #    endif
 #endif
