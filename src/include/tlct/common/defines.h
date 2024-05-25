c869dc1297a504425d9b9ac2261e5ffa326c59e8
SIGS-TZ <tz23@mails.tsinghua.edu.cn>
2024-05-25 14:24:41 +0800

    fix: redefined macros

--- src/include/tlct/common/defines.h
+++ src/include/tlct/common/defines.h
@@ -2,20 +2,24 @@
 
 #include "cmake.h"
 
-#define TLCT_API
-
-#if !defined(TLCT_HEADER_ONLY) && defined(TLCT_BUILD_SHARED_LIBS)
-#    ifdef _MSC_VER
-#        ifdef _TLCT_BUILD_LIBS
-#            define TLCT_API __declspec(dllexport)
+#ifdef TLCT_HEADER_ONLY
+#    define TLCT_API
+#else
+#    ifdef TLCT_BUILD_SHARED_LIBS
+#        ifdef _MSC_VER
+#            ifdef _TLCT_BUILD_LIBS
+#                define TLCT_API __declspec(dllexport)
+#            else
+#                define TLCT_API __declspec(dllimport)
+#            endif
 #        else
-#            define TLCT_API __declspec(dllimport)
+#            ifdef _TLCT_BUILD_LIBS
+#                define TLCT_API __attribute__((visibility("default")))
+#            else
+#                define TLCT_API
+#            endif
 #        endif
 #    else
-#        ifdef _TLCT_BUILD_LIBS
-#            define TLCT_API __attribute__((visibility("default")))
-#        else
-#            define TLCT_API
-#        endif
+#        define TLCT_API
 #    endif
 #endif
