0a2db98e7ecebd485c88096f5dca4a056e58210d
SIGS-TZ <tz23@mails.tsinghua.edu.cn>
2024-05-29 15:49:31 +0800

    refactor: polish the main function of multiview conversion

--- src/bin/dbg_mv.cpp
+++ src/bin/dbg_mv.cpp
@@ -20,29 +20,27 @@ int main(int argc, char* argv[])
     const auto layout =
         tcfg::Layout::fromCfgAndImgsize(param_cfg.getCalibCfg(), param_cfg.getImgSize()).upsample(upsample);
     auto state = tcvt::State::fromLayoutAndViews(layout, param_cfg.getViews());
-    const cv::Range range = param_cfg.getRange();
 
-    const auto initpath = ParamConfig::fmtSrcPath(param_cfg, range.start);
-    cv::Mat init = cv::imread(initpath.string());
-    state.feed(std::move(init));
+    const cv::Range range = param_cfg.getRange();
+    if (range.size() > 1) {
+        // For a better patch size estimation
+        const auto initpath = ParamConfig::fmtSrcPath(param_cfg, range.start);
+        state.feed(cv::imread(initpath.string()));
+    }
 
     for (int i = range.start; i <= range.end; i++) {
         const auto srcpath = ParamConfig::fmtSrcPath(param_cfg, i);
-
-        cv::Mat src = cv::imread(srcpath.string());
-        state.feed(std::move(src));
+        state.feed(cv::imread(srcpath.string()));
 
         int img_cnt = 1;
+        const auto dstdir = ParamConfig::fmtDstPath(param_cfg, i);
+        fs::create_directories(dstdir);
         for (const auto& mv : state) {
             std::stringstream filename_s;
             filename_s << "image_" << std::setw(3) << std::setfill('0') << img_cnt << ".png";
-            img_cnt++;
-
-            const auto dstdir = ParamConfig::fmtDstPath(param_cfg, i);
-            fs::create_directories(dstdir);
             const fs::path saveto_path = dstdir / filename_s.str();
-
             cv::imwrite(saveto_path.string(), mv);
+            img_cnt++;
         }
     }
 }
