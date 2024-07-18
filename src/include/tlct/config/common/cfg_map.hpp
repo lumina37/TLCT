8dcca3ee5b76d905f9451a978ed5bae5f587cd64
SIGS-TZ <tz23@mails.tsinghua.edu.cn>
2024-07-18 22:54:47 +0800

    feat: `fromFstream` for `ConfigMap`

--- src/include/tlct/config/common/cfg_map.hpp
+++ src/include/tlct/config/common/cfg_map.hpp
@@ -26,6 +26,7 @@ public:
     TLCT_API explicit inline ConfigMap(TMap cfg_map) : map_(std::move(cfg_map)){};
 
     // Initialize from
+    [[nodiscard]] TLCT_API static inline ConfigMap fromFstream(std::ifstream&& ifs) noexcept;
     [[nodiscard]] TLCT_API static inline ConfigMap fromPath(const std::string_view& path);
 
     // Const methods
@@ -36,23 +37,17 @@ public:
     template <typename Tv>
     [[nodiscard]] TLCT_API inline Tv get(const std::string& key) const;
     template <typename Tv>
-    [[nodiscard]] TLCT_API inline Tv get(const std::string& key, const Tv& default_val) const noexcept;
+    [[nodiscard]] TLCT_API inline Tv get(const std::string& key, const Tv& default_val) const;
 
 private:
     TMap map_;
 };
 
-ConfigMap ConfigMap::fromPath(const std::string_view& path)
+ConfigMap ConfigMap::fromFstream(std::ifstream&& ifs) noexcept
 {
-    std::ifstream fs(path.data());
-    if (!fs) {
-        std::cerr << "Failed to load `" << typeid(ConfigMap).name() << "` from `" << path << "`!" << std::endl;
-        return {};
-    }
-
     std::map<std::string, std::string> cfg_map;
     std::string row;
-    while (std::getline(fs, row)) {
+    while (std::getline(ifs, row)) {
         if (row.empty() || row.starts_with('=')) {
             break;
         }
@@ -64,6 +59,17 @@ ConfigMap ConfigMap::fromPath(const std::string_view& path)
     }
 
     return ConfigMap(cfg_map);
+}
+
+ConfigMap ConfigMap::fromPath(const std::string_view& path)
+{
+    std::ifstream ifs(path.data());
+    if (!ifs) {
+        std::cerr << "Failed to load `" << typeid(ConfigMap).name() << "` from `" << path << "`!" << std::endl;
+        return {};
+    }
+
+    return fromFstream(std::move(ifs));
 }
 
 bool ConfigMap::isEmpty() const noexcept { return map_.empty(); }
@@ -79,62 +85,47 @@ int ConfigMap::getPipelineType() const noexcept
     return ival;
 }
 
+template <typename Tv>
+inline Tv stox(const std::string& str);
+
 template <>
-int ConfigMap::get(const std::string& key, const int& default_val) const noexcept
+inline int stox(const std::string& str)
+{
+    return std::stoi(str);
+}
+
+template <>
+inline float stox(const std::string& str)
+{
+    return std::stof(str);
+}
+
+template <>
+inline double stox(const std::string& str)
+{
+    return std::stod(str);
+}
+
+template <typename Tv>
+Tv ConfigMap::get(const std::string& key, const Tv& default_val) const
 {
     const auto it = map_.find(key);
     if (it == map_.end()) {
         return default_val;
     }
     const std::string& val = it->second;
-    const int nval = std::stoi(val);
+    const float nval = stox<Tv>(val);
     return nval;
 };
 
-template <>
-int ConfigMap::get(const std::string& key) const
+template <typename Tv>
+Tv ConfigMap::get(const std::string& key) const
 {
-    return std::stoi(map_.at(key));
+    return stox<Tv>(map_.at(key));
 };
 
 template <>
-double ConfigMap::get(const std::string& key, const double& default_val) const noexcept
-{
-    const auto it = map_.find(key);
-    if (it == map_.end()) {
-        return default_val;
-    }
-    const std::string& val = it->second;
-    const double nval = std::stod(val);
-    return nval;
-};
-
-template <>
-double ConfigMap::get(const std::string& key) const
-{
-    return std::stod(map_.at(key));
-};
-
-template <>
-float ConfigMap::get(const std::string& key, const float& default_val) const noexcept
-{
-    const auto it = map_.find(key);
-    if (it == map_.end()) {
-        return default_val;
-    }
-    const std::string& val = it->second;
-    const float nval = std::stof(val);
-    return nval;
-};
-
-template <>
-float ConfigMap::get(const std::string& key) const
-{
-    return std::stof(map_.at(key));
-};
-
-template <>
-std::string ConfigMap::get(const std::string& key, const std::string& default_val) const noexcept
+std::string ConfigMap::get(const std::string& key, const std::string& default_val) const
 {
     const auto it = map_.find(key);
     if (it == map_.end()) {
