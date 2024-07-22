ac45eccc1a3d47ade7ed99c60f2b1f3eb4545596
SIGS-TZ <tz23@mails.tsinghua.edu.cn>
2024-07-22 08:36:17 +0800

    feat: use find_package prior than FetchContent

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,13 +1,15 @@
 include(FetchContent)
 
 # OpenCV
-set(TLCT_OPENCV_PATH "https://github.com/opencv/opencv.git" CACHE STRING
-        "Specifies the path of opencv (git repo or local dir)")
-set(TLCT_OPENCV_CONTRIB_PATH "https://github.com/opencv/opencv_contrib.git" CACHE STRING
-        "Specifies the path of opencv-contrib (git repo or local dir)")
+set(TLCT_OPENCV_PATH "https://github.com/OpenCV/OpenCV.git" CACHE STRING
+        "Specifies the path of OpenCV (git repo or local dir)")
+set(TLCT_OPENCV_CONTRIB_PATH "https://github.com/OpenCV/OpenCV_contrib.git" CACHE STRING
+        "Specifies the path of OpenCV-contrib (git repo or local dir)")
 
+set(OpenCV_VERSION 4.10.0)
 set(OpenCV_REQUIRED_MODULES imgcodecs imgproc quality)
 string(REPLACE ";" "," OpenCV_BUILD_LIST "${OpenCV_REQUIRED_MODULES}")
+string(REPLACE ";" " " OpenCV_FIND_PACKAGE_ARGS "${OpenCV_REQUIRED_MODULES}")
 
 set(BUILD_LIST ${OpenCV_BUILD_LIST} CACHE STRING "")
 set(BUILD_WITH_STATIC_CRT OFF CACHE BOOL "")
@@ -20,7 +22,7 @@ set(ENABLE_PRECOMPILED_HEADERS OFF CACHE BOOL "")
 set(CPU_BASELINE AVX2 CACHE STRING "")
 set(CPU_DISPATCH AVX2 CACHE STRING "")
 
-set(BUILD_opencv_apps OFF CACHE BOOL "")
+set(BUILD_OpenCV_apps OFF CACHE BOOL "")
 set(WITH_DSHOW OFF CACHE BOOL "")
 set(WITH_FFMPEG OFF CACHE BOOL "")
 set(WITH_FLATBUFFERS OFF CACHE BOOL "")
@@ -44,39 +46,39 @@ set(WITH_WEBP OFF CACHE BOOL "")
 
 if (TLCT_OPENCV_CONTRIB_PATH MATCHES "\.git$")
     FetchContent_Declare(
-            opencv_contrib
+            OpenCV_contrib
             GIT_REPOSITORY ${TLCT_OPENCV_CONTRIB_PATH}
-            GIT_TAG 4.10.0
+            GIT_TAG ${OpenCV_VERSION}
             OVERRIDE_FIND_PACKAGE
     )
 else ()
     FetchContent_Declare(
-            opencv_contrib
+            OpenCV_contrib
             DOWNLOAD_EXTRACT_TIMESTAMP ON
             URL ${TLCT_OPENCV_CONTRIB_PATH}
             OVERRIDE_FIND_PACKAGE
     )
 endif ()
-FetchContent_MakeAvailable(opencv_contrib)
+FetchContent_MakeAvailable(OpenCV_contrib)
 
-set(OPENCV_EXTRA_MODULES_PATH ${opencv_contrib_SOURCE_DIR}/modules CACHE STRING "")
+set(OPENCV_EXTRA_MODULES_PATH ${OpenCV_contrib_SOURCE_DIR}/modules CACHE STRING "")
 
 if (TLCT_OPENCV_PATH MATCHES "\.git$")
     FetchContent_Declare(
-            opencv
+            OpenCV
             GIT_REPOSITORY ${TLCT_OPENCV_PATH}
-            GIT_TAG 4.10.0
-            OVERRIDE_FIND_PACKAGE
+            GIT_TAG ${OpenCV_VERSION}
+            FIND_PACKAGE_ARGS ${OpenCV_VERSION} COMPONENTS ${OpenCV_FIND_PACKAGE_ARGS}
     )
 else ()
     FetchContent_Declare(
-            opencv
+            OpenCV
             DOWNLOAD_EXTRACT_TIMESTAMP ON
             URL ${TLCT_OPENCV_PATH}
-            OVERRIDE_FIND_PACKAGE
+            FIND_PACKAGE_ARGS ${OpenCV_VERSION} COMPONENTS ${OpenCV_FIND_PACKAGE_ARGS}
     )
 endif ()
-FetchContent_MakeAvailable(opencv)
+FetchContent_MakeAvailable(OpenCV)
 
 set(OpenCV_INCLUDE_DIR ${OPENCV_CONFIG_FILE_INCLUDE_DIR})
 set(OpenCV_LIBS ${OPENCV_MODULES_BUILD})
@@ -97,14 +99,14 @@ if (TLCT_PUGIXML_PATH MATCHES "\.git$")
             pugixml
             GIT_REPOSITORY ${TLCT_PUGIXML_PATH}
             GIT_TAG v1.14
-            OVERRIDE_FIND_PACKAGE
+            FIND_PACKAGE_ARGS
     )
 else ()
     FetchContent_Declare(
             pugixml
             DOWNLOAD_EXTRACT_TIMESTAMP ON
             URL ${TLCT_PUGIXML_PATH}
-            OVERRIDE_FIND_PACKAGE
+            FIND_PACKAGE_ARGS
     )
 endif ()
 FetchContent_MakeAvailable(pugixml)
@@ -118,7 +120,7 @@ if (TLCT_BUILD_TESTS)
             googletest
             GIT_REPOSITORY https://github.com/google/googletest.git
             GIT_TAG v1.14.0
-            OVERRIDE_FIND_PACKAGE
+            FIND_PACKAGE_ARGS
     )
     FetchContent_MakeAvailable(googletest)
 
@@ -129,7 +131,6 @@ if (TLCT_BUILD_TESTS)
             tlct-test-data
             GIT_REPOSITORY https://github.com/SIGS-TZ/TLCT-test-data.git
             GIT_TAG 0b0ee4d57b7ffba9cbd086dcac2617a07404888a
-            OVERRIDE_FIND_PACKAGE
     )
     FetchContent_MakeAvailable(tlct-test-data)
 endif ()
