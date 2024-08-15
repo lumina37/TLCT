96e3de866a087543f392d1c79db1c366c108c3b6
SIGS-TZ <tz23@mails.tsinghua.edu.cn>
2024-08-15 14:31:31 +0800

    chore: do not compile OpenCV

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,76 +1,7 @@
 include(FetchContent)
 
 # OpenCV
-set(TLCT_OpenCV_VERSION 4.10.0 CACHE STRING "Specifies the version of OpenCV")
-
-set(TLCT_OPENCV_PATH "https://github.com/opencv/opencv/archive/refs/tags/${TLCT_OpenCV_VERSION}.tar.gz" CACHE STRING
-        "Specifies the path of OpenCV")
-set(TLCT_OPENCV_CONTRIB_PATH "https://github.com/opencv/opencv_contrib/archive/refs/tags/${TLCT_OpenCV_VERSION}.tar.gz" CACHE STRING
-        "Specifies the path of OpenCV_contrib")
-
-set(OpenCV_REQUIRED_MODULES imgcodecs imgproc quality)
-string(REPLACE ";" "," OpenCV_BUILD_LIST "${OpenCV_REQUIRED_MODULES}")
-string(REPLACE ";" " " OpenCV_FIND_PACKAGE_ARGS "${OpenCV_REQUIRED_MODULES}")
-
-set(BUILD_LIST ${OpenCV_BUILD_LIST} CACHE STRING "")
-set(BUILD_WITH_STATIC_CRT OFF CACHE BOOL "")
-set(BUILD_SHARED_LIBS OFF CACHE BOOL "")
-set(CV_TRACE OFF CACHE BOOL "")
-set(OPENCV_GENERATE_SETUPVARS OFF CACHE BOOL "")
-set(OPENCV_PYTHON3_VERSION "" CACHE STRING "")
-set(ENABLE_PRECOMPILED_HEADERS OFF CACHE BOOL "")
-
-set(CPU_BASELINE AVX2 CACHE STRING "")
-set(CPU_DISPATCH AVX2 CACHE STRING "")
-
-set(BUILD_OpenCV_apps OFF CACHE BOOL "")
-set(WITH_ADE OFF CACHE BOOL "")
-set(WITH_DSHOW OFF CACHE BOOL "")
-set(WITH_FFMPEG OFF CACHE BOOL "")
-set(WITH_FLATBUFFERS OFF CACHE BOOL "")
-set(WITH_GSTREAMER OFF CACHE BOOL "")
-set(WITH_IMGCODEC_HDR OFF CACHE BOOL "")
-set(WITH_IMGCODEC_PFM OFF CACHE BOOL "")
-set(WITH_IMGCODEC_PXM OFF CACHE BOOL "")
-set(WITH_IMGCODEC_SUNRASTER OFF CACHE BOOL "")
-set(WITH_IPP OFF CACHE BOOL "")
-set(WITH_JASPER OFF CACHE BOOL "")
-set(WITH_JPEG OFF CACHE BOOL "")
-set(WITH_LAPACK OFF CACHE BOOL "")
-set(WITH_MSMF OFF CACHE BOOL "")
-set(WITH_MSMF_DXVA OFF CACHE BOOL "")
-set(WITH_OPENCL OFF CACHE BOOL "")
-set(WITH_OPENEXR OFF CACHE BOOL "")
-set(WITH_OPENJPEG OFF CACHE BOOL "")
-set(WITH_PROTOBUF OFF CACHE BOOL "")
-set(WITH_TIFF OFF CACHE BOOL "")
-set(WITH_VTK OFF CACHE BOOL "")
-set(WITH_WEBP OFF CACHE BOOL "")
-
-FetchContent_Declare(
-        OpenCV_contrib
-        DOWNLOAD_EXTRACT_TIMESTAMP ON
-        URL ${TLCT_OPENCV_CONTRIB_PATH}
-        OVERRIDE_FIND_PACKAGE
-)
-FetchContent_MakeAvailable(OpenCV_contrib)
-
-set(OPENCV_EXTRA_MODULES_PATH ${opencv_contrib_SOURCE_DIR}/modules CACHE STRING "")
-
-FetchContent_Declare(
-        OpenCV
-        DOWNLOAD_EXTRACT_TIMESTAMP ON
-        URL ${TLCT_OPENCV_PATH}
-        OVERRIDE_FIND_PACKAGE
-)
-FetchContent_MakeAvailable(OpenCV)
-
-set(OpenCV_INCLUDE_DIRS ${OPENCV_CONFIG_FILE_INCLUDE_DIR})
-set(OpenCV_LIBS ${OPENCV_MODULES_BUILD})
-
-foreach (mod ${OpenCV_LIBS})
-    list(APPEND OpenCV_INCLUDE_DIRS ${OPENCV_MODULE_${mod}_LOCATION}/include)
-endforeach ()
+find_package(OpenCV REQUIRED COMPONENTS imgproc imgcodecs quality)
 
 # pugixml
 set(TLCT_PUGIXML_VERSION 1.14 CACHE STRING "Specifies the version of pugixml")
