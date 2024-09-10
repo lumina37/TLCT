010f27418d36aa64317e49d6f69c4bb464e26d18
SIGS-TZ <tz23@mails.tsinghua.edu.cn>
2024-09-10 18:34:55 +0800

    fix: reduce the alignment to 16

--- src/include/tlct/convert/helper/microimages.hpp
+++ src/include/tlct/convert/helper/microimages.hpp
@@ -37,16 +37,15 @@ public:
     class Params
     {
     public:
-        static constexpr int CACHELINE_SIZE = 0x40;
+        static constexpr int SIMD_FETCH_SIZE = 256 / 8;
 
         TLCT_API inline Params() noexcept = default;
         TLCT_API inline explicit Params(const TLayout& layout) noexcept
         {
             idiameter_ = _hp::iround(layout.getDiameter());
-            const int mat_size = idiameter_ * idiameter_ * sizeof(float);
-            aligned_mat_size_ = _hp::align_to<CACHELINE_SIZE>(mat_size);
+            const int row_step = _hp::align_to<SIMD_FETCH_SIZE>(idiameter_ * sizeof(float));
+            aligned_mat_size_ = row_step * idiameter_;
             aligned_mi_size_ = WrapMI::CACHED_MAT_NUM * aligned_mat_size_;
-            mi_rows_ = layout.getMIRows();
             mi_max_cols_ = layout.getMIMaxCols();
             mi_num_ = mi_max_cols_ * layout.getMIRows();
             buffer_size_ = mi_num_ * aligned_mi_size_;
@@ -57,7 +56,6 @@ public:
         int idiameter_;
         int aligned_mat_size_;
         int aligned_mi_size_;
-        int mi_rows_;
         int mi_max_cols_;
         int mi_num_;
         int buffer_size_;
@@ -110,7 +108,7 @@ template <typename TLayout>
 MIs<TLayout>::MIs(const TLayout& layout) : layout_(layout), params_(layout)
 {
     items_.resize(params_.mi_num_);
-    buffer_ = std::malloc(params_.buffer_size_ + Params::CACHELINE_SIZE);
+    buffer_ = std::malloc(params_.buffer_size_ + Params::SIMD_FETCH_SIZE);
 }
 
 template <typename TLayout>
@@ -128,7 +126,7 @@ MIs<TLayout>& MIs<TLayout>::update(const cv::Mat& src)
     cv::cvtColor(src, Iu8, cv::COLOR_BGR2GRAY);
 
     auto item_it = items_.begin();
-    auto* row_cursor = (uint8_t*)_hp::align_to<Params::CACHELINE_SIZE>((size_t)buffer_);
+    auto* row_cursor = (uint8_t*)_hp::align_to<Params::SIMD_FETCH_SIZE>((size_t)buffer_);
     size_t row_step = params_.mi_max_cols_ * params_.aligned_mi_size_;
     for (const int irow : rgs::views::iota(0, layout_.getMIRows())) {
 
