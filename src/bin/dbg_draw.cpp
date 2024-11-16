3a93cebe279b1d33e65afd0183958aec1e39504e
lumina37 <starry.qvq@gmail.com>
2024-11-16 17:34:28 +0800

    feat: re-enable dbg draw centers

--- src/bin/dbg_draw.cpp
+++ src/bin/dbg_draw.cpp
@@ -5,27 +5,31 @@
 
 #include "tlct.hpp"
 
+namespace fs = std::filesystem;
 namespace rgs = std::ranges;
 
 int main(int argc, char* argv[])
 {
-    const auto cfg_map = tlct::ConfigMap::fromPath(argv[1]);
+    const fs::path case_root{argv[1]};
+    const auto cfgpath = case_root / "param.cfg";
+    const auto cfgmap = tlct::ConfigMap::fromPath(cfgpath.string());
     cv::Mat resized_img;
 
-    if (cfg_map.getPipelineType() == tlct::PipelineType::TLCT) {
+    if (cfgmap.getPipelineType() == tlct::PipelineType::TLCT) {
         namespace tn = tlct::tspc;
 
-        const auto param_cfg = tn::ParamConfig::fromConfigMap(cfg_map);
+        const auto param_cfg = tn::ParamConfig::fromConfigMap(cfgmap);
         const auto layout = tn::Layout::fromCalibAndSpecConfig(param_cfg.getCalibCfg(), param_cfg.getSpecificCfg());
         const auto& generic_cfg = param_cfg.getGenericCfg();
 
-        const auto srcpath = generic_cfg.fmtSrcPath(generic_cfg.getRange().start);
+        const auto srcpath = case_root / "example.png";
         const cv::Mat src = cv::imread(srcpath.string());
         layout.processInto(src, resized_img);
 
         for (const int row : rgs::views::iota(0, layout.getMIRows())) {
             for (const int col : rgs::views::iota(0, layout.getMICols(row))) {
                 const auto center = layout.getMICenter(row, col);
+                cv::circle(resized_img, center, 1, {0, 0, 255}, 1, cv::LINE_AA);
                 cv::circle(resized_img, center, tlct::_hp::iround(layout.getRadius()), {0, 0, 255}, 1, cv::LINE_AA);
             }
         }
@@ -49,11 +53,11 @@ int main(int argc, char* argv[])
     } else {
         namespace tn = tlct::raytrix;
 
-        const auto param_cfg = tn::ParamConfig::fromConfigMap(cfg_map);
+        const auto param_cfg = tn::ParamConfig::fromConfigMap(cfgmap);
         const auto layout = tn::Layout::fromCalibAndSpecConfig(param_cfg.getCalibCfg(), param_cfg.getSpecificCfg());
         const auto& generic_cfg = param_cfg.getGenericCfg();
 
-        const auto srcpath = generic_cfg.fmtSrcPath(generic_cfg.getRange().start);
+        const auto srcpath = case_root / "example.png";
         const cv::Mat src = cv::imread(srcpath.string());
         layout.processInto(src, resized_img);
 
@@ -66,6 +70,7 @@ int main(int argc, char* argv[])
                 const bool b = mitype == 2;
                 cv::circle(resized_img, center, tlct::_hp::iround(layout.getRadius()),
                            {255.0 * b, 255.0 * g, 255.0 * r}, 1, cv::LINE_AA);
+                cv::circle(resized_img, center, 1, {255.0 * b, 255.0 * g, 255.0 * r}, 1, cv::LINE_AA);
             }
         }
 
@@ -83,5 +88,6 @@ int main(int argc, char* argv[])
         }
     }
 
-    cv::imwrite("dbg_center.png", resized_img);
+    const auto dstpath = case_root / "center.png";
+    cv::imwrite(dstpath.string(), resized_img);
 }
