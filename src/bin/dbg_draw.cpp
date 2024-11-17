fa39c4acb882761da3bf2eed84ab26a475a36401
lumina37 <starry.qvq@gmail.com>
2024-11-17 13:34:53 +0800

    chore: dbg draw

--- src/bin/dbg_draw.cpp
+++ src/bin/dbg_draw.cpp
@@ -20,7 +20,6 @@ int main(int argc, char* argv[])
 
         const auto param_cfg = tn::ParamConfig::fromConfigMap(cfgmap);
         const auto layout = tn::Layout::fromCalibAndSpecConfig(param_cfg.getCalibCfg(), param_cfg.getSpecificCfg());
-        const auto& generic_cfg = param_cfg.getGenericCfg();
 
         const auto srcpath = case_root / "example.png";
         const cv::Mat src = cv::imread(srcpath.string());
@@ -29,33 +28,15 @@ int main(int argc, char* argv[])
         for (const int row : rgs::views::iota(0, layout.getMIRows())) {
             for (const int col : rgs::views::iota(0, layout.getMICols(row))) {
                 const auto center = layout.getMICenter(row, col);
-                cv::circle(resized_img, center, 1, {0, 0, 255}, 1, cv::LINE_AA);
-                cv::circle(resized_img, center, tlct::_hp::iround(layout.getRadius()), {0, 0, 255}, 1, cv::LINE_AA);
+                resized_img.at<cv::Vec3b>(center) = {0, 0, 255};
+                cv::circle(resized_img, center, tlct::_hp::iround(layout.getRadius()), {255, 0, 0}, 1, cv::LINE_AA);
             }
-        }
-        for (const int col : rgs::views::iota(0, layout.getMICols(0))) {
-            const auto center = layout.getMICenter(0, col);
-            cv::circle(resized_img, center, tlct::_hp::iround(layout.getRadius()), {255, 0, 0}, 1, cv::LINE_AA);
-        }
-
-        const cv::Scalar base_color{0, 255.0 / 6, 255.0 / 6};
-        auto neighbors = tn::Neighbors::fromLayoutAndIndex(layout, {4, 3});
-        for (const auto direction : tn::Neighbors::DIRECTIONS) {
-            cv::circle(resized_img, neighbors.getNeighborPt(direction), tlct::_hp::iround(layout.getRadius()),
-                       base_color * (int)direction, 2, cv::LINE_AA);
-        }
-
-        neighbors = tn::Neighbors::fromLayoutAndIndex(layout, {4, 8});
-        for (const auto direction : tn::Neighbors::DIRECTIONS) {
-            cv::circle(resized_img, neighbors.getNeighborPt(direction), tlct::_hp::iround(layout.getRadius()),
-                       base_color * (int)direction, 2, cv::LINE_AA);
         }
     } else {
         namespace tn = tlct::raytrix;
 
         const auto param_cfg = tn::ParamConfig::fromConfigMap(cfgmap);
         const auto layout = tn::Layout::fromCalibAndSpecConfig(param_cfg.getCalibCfg(), param_cfg.getSpecificCfg());
-        const auto& generic_cfg = param_cfg.getGenericCfg();
 
         const auto srcpath = case_root / "example.png";
         const cv::Mat src = cv::imread(srcpath.string());
@@ -70,21 +51,8 @@ int main(int argc, char* argv[])
                 const bool b = mitype == 2;
                 cv::circle(resized_img, center, tlct::_hp::iround(layout.getRadius()),
                            {255.0 * b, 255.0 * g, 255.0 * r}, 1, cv::LINE_AA);
-                cv::circle(resized_img, center, 1, {255.0 * b, 255.0 * g, 255.0 * r}, 1, cv::LINE_AA);
+                resized_img.at<cv::Vec3b>(center) = {(uint8_t)(255 * b), (uint8_t)(255 * g), (uint8_t)(255 * r)};
             }
-        }
-
-        const cv::Scalar base_color{0, 255.0 / 6, 255.0 / 6};
-        auto neighbors = tn::NearNeighbors::fromLayoutAndIndex(layout, {4, 3});
-        for (const auto direction : tn::NearNeighbors::DIRECTIONS) {
-            cv::circle(resized_img, neighbors.getNeighborPt(direction), tlct::_hp::iround(layout.getRadius()),
-                       base_color * (int)direction, 2, cv::LINE_AA);
-        }
-
-        auto far_neighbors = tn::FarNeighbors::fromLayoutAndIndex(layout, {4, 8});
-        for (const auto direction : tn::FarNeighbors::DIRECTIONS) {
-            cv::circle(resized_img, far_neighbors.getNeighborPt(direction), tlct::_hp::iround(layout.getRadius()),
-                       base_color * (int)direction, 2, cv::LINE_AA);
         }
     }
 
