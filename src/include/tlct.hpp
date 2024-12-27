ee79fe0f95f2dad9b2d03884a43c25c2170d2ca5
lumina37 <starry.qvq@gmail.com>
2024-12-27 20:57:38 +0800

    feat: always deps on argparse

--- src/include/tlct.hpp
+++ src/include/tlct.hpp
@@ -6,19 +6,10 @@
 #include "tlct/io.hpp"
 #include "tlct/namespace.hpp"
 
-#ifdef TLCT_BUILD_APP
-
 namespace tlct {
 
 using cfg::CliConfig;
-using cfg::makeParser;
-
-} // namespace tlct
-
-#endif
-
-namespace tlct {
-
 using cfg::ConfigMap;
+using cfg::makeParser;
 
 } // namespace tlct
--- src/include/tlct/config/common.hpp
+++ src/include/tlct/config/common.hpp
@@ -1,26 +1,14 @@
 #pragma once
 
-#include "tlct/common/defines.h"
-
-#ifdef TLCT_BUILD_APP
-
-#    include "tlct/config/common/cli.hpp"
+#include "tlct/config/common/cli.hpp"
+#include "tlct/config/common/map.hpp"
 
 namespace tlct::cfg {
 
 namespace _ = _cfg;
 
 using _::CliConfig;
+using _::ConfigMap;
 using _::makeParser;
-
-} // namespace tlct::cfg
-
-#endif
-
-#include "tlct/config/common/map.hpp"
-
-namespace tlct::cfg {
-
-using _cfg::ConfigMap;
 
 } // namespace tlct::cfg
