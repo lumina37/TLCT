e06e1c977330037b3fac5c7cb821182518119a3b
lumina37 <starry.qvq@gmail.com>
2025-06-03 07:36:57 +0800

    chore: remove include dir

--- src/include/tlct/common/info.cpp
+++ src/include/tlct/common/info.cpp
@@ -1,4 +1,3 @@
-#include <filesystem>
 #include <string>
 
 #include "tlct/common/config.h"
@@ -9,10 +8,7 @@
 
 namespace tlct {
 
-namespace fs = std::filesystem;
-
 const std::string_view compileInfo{TLCT_COMPILE_INFO};
 const std::string_view version{TLCT_VERSION};
-const fs::path includeBase{TLCT_INCLUDE_BASE};
 
 }  // namespace tlct
--- src/include/tlct/common/info.hpp
+++ src/include/tlct/common/info.hpp
@@ -1,17 +1,13 @@
 #pragma once
 
-#include <filesystem>
 #include <string>
 
 #include "tlct/common/defines.h"
 
 namespace tlct {
 
-namespace fs = std::filesystem;
-
 TLCT_API extern const std::string_view compileInfo;
 TLCT_API extern const std::string_view version;
-TLCT_API extern const fs::path includeBase;
 
 }  // namespace tlct
 
