7e4d2d2cf299a57163a2bb5d6e390232ddadb86f
lumina37 <starry.qvq@gmail.com>
2025-12-08 15:50:30 +0800

    fix: large psize

--- src/include/tlct/convert/patchsize/ssim/params.cpp
+++ src/include/tlct/convert/patchsize/ssim/params.cpp
@@ -1,5 +1,6 @@
 #include "tlct/config/arrange.hpp"
 #include "tlct/config/concepts.hpp"
+#include "tlct/convert/helper/consts.hpp"
 #include "tlct/helper/constexpr/math.hpp"
 #include "tlct/helper/error.hpp"
 #include "tlct/helper/std.hpp"
@@ -16,9 +17,17 @@ auto PsizeParams_<TArrange>::create(const TArrange& arrange, const TCvtConfig& c
     constexpr float PATTERN_SIZE = 0.35f;
 
     const float patternSize = arrange.getDiameter() * PATTERN_SIZE;
-    const float radius = arrange.getDiameter() / 2.f;
+    const float safeDiameter = arrange.getDiameter() * CONTENT_SAFE_RATIO;
+
+    const float safeRadius = safeDiameter / 2.f;
     const float halfPatternSize = patternSize / 2.f;
-    const float patternShift = std::sqrt((radius - halfPatternSize) * (radius + halfPatternSize)) - halfPatternSize;
+    const float safePatternShift =
+        std::sqrt((safeRadius - halfPatternSize) * (safeRadius + halfPatternSize)) - halfPatternSize;
+
+    const float maxPsizeRatio = (1.f - cvtCfg.viewShiftRange) * CONTENT_SAFE_RATIO / cvtCfg.psizeInflate;
+    const float candiPatternShift = maxPsizeRatio * safeDiameter / 2.f;
+
+    const float patternShift = std::min(safePatternShift, candiPatternShift);
 
     const int minPsize = _hp::iround(0.5f * patternSize);
 
